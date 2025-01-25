37780fd2f2f8ea024e0e8211ff3516460b2ad85b
Piotrus Watson <piotruswatson@gmail.com>
2025-01-25 02:20:07 +0200

    added a stripped down balloon in. yet to try making it work

---
+++ addons/dialogue_manager/example_balloon/text_ballon.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+@onready var text_holder = $Balloon/Panel/Dialogue/VBoxContainer/Textholder
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func update_text(text):
+	wipe_text()
+	display_text(text)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+### this is very expensive! make this less expensive by making a pool of buttons
+### when we have a more clear idea of the size the text fits into
+func display_text(text):
+	for word in text.split(" "):
+		var button = Button.new()
+		button.text = word
+		# setup script to take words here
+		text_holder.add_child(button)
+		
+func wipe_text():
+	for child in text_holder.get_children():
+		text_holder.remove_child(child)
+		child.queue_free()
