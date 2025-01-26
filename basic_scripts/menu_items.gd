ca37d3092cfb9636411711c0c76d6f03854114f7
Piotrus Watson <piotruswatson@gmail.com>
2025-01-26 13:12:37 +0200

    what

--- basic_scripts/menu_items.gd
+++ basic_scripts/menu_items.gd
@@ -27,8 +27,24 @@ func find_words_in_key(key, order):
 		if words_to_found.has(item):
 			words_to_found[item] = true
 	return words_to_found
+
+func is_order_complete(words_to_found):
+	var found = true
+	for word in words_to_found:
+		found = found and words_to_found[word]
+	return found
+
+func is_order_incomplete(words_to_found):
+	for word in words_to_found:
+		if words_to_found[word]:
+			return true
+	return false
 	
-func 
+func get_item_from_info(words_to_found):
+	var item = []
+	for word in words_to_found:
+		item.append(word)
+	return menu[item]
 
 func process_order(order):
 	if !order_is_relevant(order):
@@ -41,6 +57,16 @@ func process_order(order):
 	var found_info = []
 	for key in keys:
 		found_info.append(find_words_in_key(key, synonymd_order))
+	var incomplete_order = null
+	for info in found_info:
+		if is_order_complete(info):
+			return {"status": Results.COMPLETE, "item": get_item_from_info(info)}
+		if is_order_incomplete(info):
+			incomplete_order = info
+	
+	if incomplete_order != null:
+		return {"status": Results.INCOMPLETE, "item": get_item_from_info(incomplete_order)}
+	return {"status": Results.FAIL, "item": null}
 	
 	
 	
