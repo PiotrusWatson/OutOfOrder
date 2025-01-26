5aa80b08ff17d248da4b5ea91340ba6dcf3440a7
theasgaardian <theasgaardian1@gmail.com>
2025-01-26 12:44:19 +0200

    instructions tied to menu

--- menu.gd
+++ menu.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://worlds/placeholder_world.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://worlds/MainMenu.tscn") # Replace with function body.
