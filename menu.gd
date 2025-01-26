8c8e924f38e84bc27103329f5d2476fcafe8c4ad
theasgaardian <theasgaardian1@gmail.com>
2025-01-26 13:28:52 +0200

    stylized instructions

--- menu.gd
+++ menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
 
+
+
 func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://worlds/MainMenu.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://worlds/MainMenu.tscn")
